300b3ddcf70c4ac00cc5e3ca212054a4dfe6c4d9
Jason Teilliez <jason.teilliez@gmail.com>
2025-03-08 19:02:43 +0100

    add scale add espace

--- game/code/main.py
+++ game/code/main.py
@@ -6,13 +6,18 @@ from player import Player
 class Game:
     def __init__(self):
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGTH))
+        self.display_surface = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        display_width = self.display_surface.get_width()
+        display_height = self.display_surface.get_height()
+
+        self.scale = min(display_width/GAME_WIDTH, display_height/GAME_HEIGTH)
+
         pygame.display.set_caption('Space Shooter')
 
         self.visible_sprites = pygame.sprite.Group()
         self.obstacle_sprites = pygame.sprite.Group()
 
-        self.player = Player(self.visible_sprites)
+        self.player = Player(self.visible_sprites, self.scale)
         self.last_time = time()
 
     def run(self):
@@ -24,9 +29,13 @@ class Game:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-
+                        pygame.quit()
+                        sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+                
             self.display_surface.fill('gray')
             self.visible_sprites.update(dt = dt)
             self.visible_sprites.draw(self.display_surface)
--- game/code/player.py
+++ game/code/player.py
@@ -4,15 +4,18 @@ from settings import *
 from math import degrees, atan2
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, groups):
+    def __init__(self, groups, scale):
         super().__init__(groups)
+        self.scale = scale
 
         basedir = path.dirname(path.dirname(__file__))
         path_to_image = path.join(basedir, "graphics", "player", "player.png")
 
-        self.surf = pygame.image.load(path_to_image).convert_alpha()
-        self.image = self.surf
-        self.rect = self.image.get_rect(center = (WINDOW_WIDTH/2,WINDOW_HEIGTH/2))
+        display_surface = pygame.display.get_surface()
+        surf = pygame.image.load(path_to_image).convert_alpha()
+        self.scale_surf = pygame.transform.scale(surf,pygame.math.Vector2(surf.get_size()) * self.scale)
+        self.image = self.scale_surf
+        self.rect = self.image.get_rect(center = (display_surface.get_width()/2,display_surface.get_height()/2))
         self.pos =  pygame.math.Vector2(self.rect.center)
 
         self.direction = pygame.math.Vector2()
@@ -41,8 +44,8 @@ class Player(pygame.sprite.Sprite):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
 
-        self.pos.x += self.direction.x * self.speed * dt
-        self.pos.y += self.direction.y * self.speed * dt
+        self.pos.x += self.direction.x * self.speed * dt * self.scale
+        self.pos.y += self.direction.y * self.speed * dt * self.scale
 
         self.rect.centerx = round(self.pos.x)
         self.rect.centery = round(self.pos.y)
@@ -56,7 +59,7 @@ class Player(pygame.sprite.Sprite):
 
         angle = - degrees(atan2(dx,-dy))
 
-        self.image = pygame.transform.rotate(self.surf, angle)
+        self.image = pygame.transform.rotate(self.scale_surf, angle)
         self.rect = self.image.get_rect(center=self.rect.center) 
     
 
